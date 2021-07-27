21edc2a7e3d1814b59bb33c95d916ad7bf1686d7
praiseopara <cpopara2010@yahoo.com>
2021-07-27 03:00:10 -0500

    connected server to sql and queried database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// import mysql2 package
+const mysql = require('mysql2');
 // import express
 const express = require('express');
 //PORT designation
@@ -8,15 +10,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// test the connection
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'hello world'
-//     });
-// });
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'mysqlpassword1$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
-
+// query database to test connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // last route & default response for any other response (not found)
 app.use((req, res) => {
